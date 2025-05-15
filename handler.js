6b5ff12176179578bb2ff6144872d28b0f55fecb
SankaW <sdweerathunga5@gmail.com>
2025-05-15 13:01:04 -0400

    Create More CRUD operation, delete, GetAll and getById

--- handler.js
+++ handler.js
@@ -14,3 +14,26 @@ module.exports.updateNote = async (event) => {
     body: JSON.stringify(`The note with id ${noteId} is updated`),
   };
 };
+
+module.exports.deleteNote = async (event) => {
+  const noteId = event.pathParameters.id;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`The note with id ${noteId} is deleted`),
+  };
+};
+
+module.exports.getAllNotes = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify("All notes are returned"),
+  };
+};
+
+module.exports.getNoteById = async (event) => {
+  const noteId = event.pathParameters.id;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`The note with id ${noteId} is returned`),
+  };
+};
