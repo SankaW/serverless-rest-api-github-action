047aae2c569953f77be06df4da02fc0249a104cd
SankaW <sdweerathunga5@gmail.com>
2025-05-15 16:16:59 -0400

    setting up aws sdk for js v3 & create item in the dynamodb table created with iam permission

--- handler.js
+++ handler.js
@@ -1,10 +1,34 @@
 "use strict";
+const crypto = require("crypto");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const client = new DynamoDBClient({ region: "us-east-1" });
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const docClient = DynamoDBDocumentClient.from(client);
 
 module.exports.createNote = async (event) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify("A new note is created"),
-  };
+  let data = JSON.parse(event.body);
+  try {
+    await docClient.send(
+      new PutCommand({
+        TableName: "notes",
+        Item: {
+          notesId: crypto.randomUUID(),
+          title: data.title,
+          body: data.body,
+        },
+        ConditionExpression: "attribute_not_exists(notesId)",
+      })
+    );
+    return {
+      statusCode: 201,
+      body: JSON.stringify("A new note is created"),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error.message),
+    };
+  }
 };
 
 module.exports.updateNote = async (event) => {
